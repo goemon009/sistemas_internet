76cdc6148a387ad5973b212632db338d09829731
goemon009 <leandro.mendes@estudante.ifmt.edu.br>
2025-06-04 18:14:06 -0400

    Create ImpostoDeRenda.java

---
+++ exercicios_programacao_desktop/ImpostoDeRenda.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class ImpostoDeRenda {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        double salario;
+
+        System.out.println("Digite o seu salário: ");
+        salario = scanner.nextDouble();
+
+
+    }
+
+    public static double imposto(double salario){
+        double imposto = 0;
+        if(salario > 4500.00){
+            imposto = imposto + (salario - 4500.00) * 0.28;
+            salario = 4500.00;
+        } else if (salario > 3000.00){
+            imposto = imposto + (salario - 3000.00) * 0.18;
+            salario = 3000.00;
+        } else if (salario > 2000){
+            imposto = imposto + (salario - 2000.00) * 0.08;
+        }
+        return imposto;
+    }
+}
